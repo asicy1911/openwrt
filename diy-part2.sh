fe6153e2a179e3bf7e1ebd6f607d63fbf82dab20
asicy1911 <asicy1986@gmail.com>
2022-01-12 12:22:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#theme
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.0/g' package/base-files/files/bin/config_generate
